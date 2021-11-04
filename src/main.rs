6ea44f817accc488ac4d9653201f875c43cc089b
Yannis Matezki <git@yannismate.de>
2021-11-05 00:06:26 +0100

    Redo parallelization

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,7 @@
-use std::error::Error;
-use std::fmt::{Debug, Display, Formatter};
+use std::collections::{HashMap, VecDeque};
 use std::net::{IpAddr, Ipv4Addr, SocketAddr, UdpSocket};
-use std::sync::Arc;
-use std::time::Duration;
+use std::sync::{Arc, Condvar, Mutex};
 use cidr_utils::cidr::{IpCidr, Ipv4Cidr};
-use rayon::{ThreadPool, ThreadPoolBuilder};
-
-struct AppState {
-  pub cfg: GeoDnsProxyCfg,
-  pub threads: ThreadPool
-}
 
 struct GeoDnsProxyCfg {
   pub geo_zones: Vec<GeoZone>
@@ -21,6 +13,8 @@ struct GeoZone {
   pub nameserver: IpAddr
 }
 
+type ProxiedPacket = ([u8; 512], usize, SocketAddr);
+
 fn main() {
 
   let z1 = GeoZone{
@@ -30,71 +24,109 @@ fn main() {
   };
 
   let cfg = GeoDnsProxyCfg{geo_zones: vec![z1]};
-  //Automatically use 1 thread per logical cpu core
-  let threads = ThreadPoolBuilder::new().build().expect("Could not start thread pool");
+  let cfg = Arc::new(cfg);
 
-  let state = Arc::new(AppState {
-    cfg,
-    threads
-  });
+  let in_socket = UdpSocket::bind("127.0.0.1:53").expect("Could not bind port");
+  let out_socket = UdpSocket::bind("0.0.0.0:0").expect("Could not bind outgoing port");
 
-  let sock = UdpSocket::bind("127.0.0.1:53").expect("Could not bind port");
+  let in_socket_cl = in_socket.try_clone().expect("Could not clone main socket");
+  let out_socket_cl = out_socket.try_clone().expect("Could not clone outgoing socket");
 
-  loop {
-    handle_incoming(&sock, &state).unwrap_or_else(|err| println!("Error while handling packet: {:?}", err));
-  }
+  let outgoing_queue = Arc::new((Mutex::new(VecDeque::<ProxiedPacket>::new()), Condvar::new()));
+  let response_queue = Arc::new((Mutex::new(VecDeque::<ProxiedPacket>::new()), Condvar::new()));
 
-}
+  let outgoing_queue_cl = outgoing_queue.clone();
+  let response_queue_cl = response_queue.clone();
 
+  let id_addr_map = Arc::new(Mutex::new(HashMap::<u16, SocketAddr>::new()));
+  let id_addr_map_cl = id_addr_map.clone();
 
-fn handle_incoming(sock: &UdpSocket, state: &AppState) -> Result<(), Box<dyn Error>> {
-  let mut buf = [0; 512];
-  let (amt, src) = sock.recv_from(&mut buf)?;
+  //Thread to write incoming socket
+  std::thread::spawn(move || {
+    let socket = in_socket_cl;
+    loop {
 
+      let mut packet = loop {
+        let mut queue = response_queue_cl.0.lock().unwrap();
+        match queue.pop_front() {
+          Some(packet) => break packet,
+          None => queue = response_queue_cl.1.wait(queue).unwrap()
+        }
+      };
 
-  let smallest_zone = state.cfg.geo_zones.iter()
-    .filter(|z| {z.cidr.contains(src.ip())})
-    .min_by_key(|z| {z.cidr.size()})
-    .ok_or(NoMatchingZoneError{})?;
+      let out_buf = &mut packet.0[..packet.1];
+      socket.send_to(out_buf, packet.2).unwrap_or_else(|err| {println!("[ERR] Responding DNS packet: {:?}", err); 0});
 
-  println!("Request from {} is in GeoZone {}. Proxying to {}...", src.ip(), smallest_zone.name, smallest_zone.nameserver);
-
-  let ns_addr = smallest_zone.nameserver;
-
-  let sock_cl = sock.try_clone()?;
-  state.threads.spawn(move || {
-    //Reduce buffer size to actual content length
-    let buf = &mut buf[..amt];
-
-    //Automatically assign port
-    let proxy_sock = UdpSocket::bind("0.0.0.0:0").unwrap();
-    proxy_sock.set_read_timeout(Some(Duration::from_secs(1))).unwrap();
-    proxy_sock.send_to(buf, SocketAddr::new(ns_addr, 53)).unwrap();
-    let mut rec_buf = [0; 512];
-    while let Ok((amt_rec, _src_rec)) = proxy_sock.recv_from(&mut rec_buf) {
-      let rec_buf = &mut rec_buf[..amt_rec];
-      sock_cl.send_to(rec_buf, src).expect("Could not forward response");
     }
   });
 
-  Ok(())
+  //Thread to read from outgoing socket
+  std::thread::spawn(move || {
+    let socket = out_socket_cl;
 
-}
+    loop {
+      let mut buf = [0; 512];
+      let (amt, _src) = socket.recv_from(&mut buf).expect("");
 
+      let packet_id = get_packet_id(&buf);
+      let mut mapping = id_addr_map_cl.lock().unwrap();
+      if mapping.contains_key(&packet_id) {
+        let addr = mapping.remove(&packet_id).unwrap();
+        response_queue.0.lock().unwrap().insert(0, (buf, amt, addr));
+        response_queue.1.notify_one();
+      } else {
+        println!("[WARN] Could not find return address for id {}", &packet_id);
+      }
+    }
+  });
 
+  //Thread to write to outgoing socket
+  std::thread::spawn(move || {
+    let out_socket = out_socket;
 
-struct NoMatchingZoneError;
+    loop {
 
-impl Debug for NoMatchingZoneError {
-  fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-    f.write_str("No matching zone!")
+      let mut packet = loop {
+        let mut queue = outgoing_queue.0.lock().unwrap();
+        match queue.pop_front() {
+          Some(packet) => break packet,
+          None => queue = outgoing_queue.1.wait(queue).unwrap()
+        }
+      };
+      let addr_ns = get_ns_addr(&cfg.geo_zones, packet.2.ip());
+      match addr_ns {
+        None => println!("[WARN] Could not find GeoZone for packet from {}", packet.2),
+        Some(addr) => {
+          let out_buf = &mut packet.0[..packet.1];
+          let packet_id = get_packet_id(&out_buf);
+          id_addr_map.lock().unwrap().insert(packet_id, packet.2);
+          out_socket.send_to(out_buf, SocketAddr::new(addr, 53)).unwrap_or_else(|err| {println!("[ERR] Outgoing DNS packet: {:?}", err); 0});
+        }
+      }
+
+    }
+  });
+
+  //Read from incoming socket
+  loop {
+    let mut buf = [0; 512];
+    let (amt, src) = in_socket.recv_from(&mut buf).expect("");
+
+    let mut queue = outgoing_queue_cl.0.lock().unwrap();
+    queue.insert(0, (buf, amt, src));
+    outgoing_queue_cl.1.notify_one();
   }
+
 }
 
-impl Display for NoMatchingZoneError {
-  fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-    f.write_str("No matching zone!")
-  }
+fn get_ns_addr(zones: &Vec<GeoZone>, addr: IpAddr) -> Option<IpAddr> {
+  zones.iter()
+    .filter(|z| {z.cidr.contains(addr)})
+    .min_by_key(|z| {z.cidr.size()})
+    .and_then(|z| {println!("[INFO] Packet from {} routed to zone {}", addr, z.name); Some(z.nameserver)})
 }
 
-impl Error for NoMatchingZoneError {}
+#[inline]
+fn get_packet_id(packet: &[u8]) -> u16 {
+  ((packet[0] as u16) << 8) | packet[1] as u16
+}
